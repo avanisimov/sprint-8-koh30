89d656fb25793380eec5381ce9a027aa13bf13bc
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2025-05-19 19:17:39 +0300

    compileSdk & targetSdk up to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.practicum.sprint8koh30"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.practicum.sprint8koh30"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
